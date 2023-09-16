0fb2a2076efc278e3bcfaf6c56d226588d3f83b5
Hajj53 <55025756+Hajj53@users.noreply.github.com>
2023-09-16 20:25:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const wrapper = document.querySelector('.wrapper')
+const loginLink = document.querySelector('.login-link')
+const RegisterLink = document.querySelector('.Register-link')
+
+
+.loginLink.addEventListener('Click', ()=> {wrapper.classList.add('active');});
+.RegisterLink.removeEventListener('Click', ()=> {wrapper.classList.add('active');});
